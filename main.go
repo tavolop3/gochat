4a5a508816b86aac816fcc2f95a516831e49d23a
tavo <tavo.lop33@gmail.com>
2025-09-16 18:45:30 -0300

    estructura simplificada

---
+++ main.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+	"github.com/gorilla/websocket"
+)
+
+var upgrader = websocket.Upgrader{
+	ReadBufferSize: 1024,
+	WriteBufferSize: 1024,
+}
+
+func main() {
+	fmt.Println("Starting server on port 8080...")
+
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		http.ServeFile(w, r, "websockets.html")
+	})
+
+	http.ListenAndServe(":8080", nil)
+}
