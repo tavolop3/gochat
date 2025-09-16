921927f16159e13570627679741b9385fc72e515
tavo <tavo.lop33@gmail.com>
2025-09-16 19:04:04 -0300

    echo

--- main.go
+++ main.go
@@ -11,12 +11,35 @@ var upgrader = websocket.Upgrader{
 	WriteBufferSize: 1024,
 }
 
+func messageHandler(w http.ResponseWriter, r *http.Request) {
+	conn, err:= upgrader.Upgrade(w, r, nil)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	for {
+		messageType, p, err := conn.ReadMessage()
+		if err != nil {
+			fmt.Println(err)
+			return
+		}
+
+		if err := conn.WriteMessage(messageType, p); err != nil {
+			fmt.Println(err)
+			return
+		}
+	}
+}
+
 func main() {
 	fmt.Println("Starting server on port 8080...")
 
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		http.ServeFile(w, r, "websockets.html")
 	})
+
+	http.HandleFunc("/msg", messageHandler)
 
 	http.ListenAndServe(":8080", nil)
 }
